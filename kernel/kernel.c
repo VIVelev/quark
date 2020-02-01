274e722c9cf33b7b5f4178637e1767656ffa7388
Victor Velev <victorivelev@gmail.com>
2020-02-01 21:18:37 +0200

    make kernel's size bigger

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,9 @@
 
 void main() {
     clear_screen();
-    kprint("Hello World!\n");
-    kprint("Goodbye World!\n");
+    kprint_at("X", 6, 1);
+    kprint_at("This text spans multiple lines", 10, 75);
+    kprint_at("There is a line\nbreak", 20, 0);
+    kprint("There is a line\nbreak");
+    kprint_at("What happens when we run out of space?", 24, 45);
 }
