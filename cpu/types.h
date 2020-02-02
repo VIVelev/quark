a850a77e956b98ede167c0cb37e3c1f3227e2319
Victor Velev <victorivelev@gmail.com>
2020-02-02 06:42:19 +0200

    [Update] cpu/types with proper and accurate unsigned types from <stdint.h>

--- cpu/types.h
+++ cpu/types.h
@@ -1,11 +1,10 @@
 #ifndef TYPES_H
     #define TYPES_H
 
-    typedef unsigned char  uint8_t;
-    typedef          char  sint8_t;
-    typedef unsigned short uint16_t;
-    typedef          short sint16_t;
-    typedef unsigned int   uint32_t;
-    typedef          int   sint32_t;
+    #include <stdint.h>
+
+    typedef char  sint8_t;
+    typedef short sint16_t;
+    typedef int   sint32_t;
 
 #endif /* TYPES_H */
