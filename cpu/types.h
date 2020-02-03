a8372fca4436abd0db473354b82aa4b6c214147a
Victor Velev <victorivelev@gmail.com>
2020-02-03 06:38:05 +0200

    [Fix] properly define signed integer types

--- cpu/types.h
+++ cpu/types.h
@@ -3,8 +3,9 @@
 
     #include <stdint.h>
 
-    typedef char  sint8_t;
-    typedef short sint16_t;
-    typedef int   sint32_t;
+    typedef int8_t sint8_t;
+    typedef int16_t sint16_t;
+    typedef int32_t sint32_t;
+    typedef int64_t sint64_t;
 
 #endif /* TYPES_H */
